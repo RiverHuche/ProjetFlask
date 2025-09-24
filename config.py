5be623813294be9a313eb8adbf781ff40eb3f024
Huche River <o22401087@or-iut-i207-f21.iut45.univ-orleans.fr>
2025-09-24 14:35:08 +0200

    Ajout du fichier config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import random, string, os
-"".join([random.choice(string.printable) for _ in os.urandom(24)])
+#"".join([random.choice(string.printable) for _ in os.urandom(24)])
 SECRET_KEY="2lzUl{$*D6#`8uXqlU."
 ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "Pour tout autres questions, veuillez nous laisser un message via notre adresse mail : bonjour@iutO.fr"
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
