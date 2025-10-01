8b1c55cd3e828282a4b85156ea41c9d67a3ef8b1
Huche River <o22401087@or-iut-i207-f21.iut45.univ-orleans.fr>
2025-10-01 15:29:43 +0200

    Ajout des tableau auteur et livres, grace au valeur de la BD

--- monApp/views.py
+++ monApp/views.py
@@ -1,11 +1,16 @@
 from .app import app
-from flask import render_template
+from flask import render_template, request
 from config import *
+from monApp.models import Auteur ,Livre
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template("index.html",title="R3.01 Dev Web avec Flask", name="Cricri")
+    if len(request.args)==0:
+        return render_template("index.html",title="R3.01 Dev Web avec Flask", name="Cricri")
+    else:
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name = param_name)
 
 
 @app.route('/about/')
@@ -15,6 +20,16 @@ def about():
 @app.route('/contact/')
 def contact():
     return render_template("contact.html",title="Nous contacter")
+
+@app.route('/auteurs/')
+def getAuteurs():
+    lesAuteurs = Auteur.query.all() 
+    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
+
+@app.route('/livres/')
+def getLivres():
+    lesLivres = Livre.query.all() 
+    return render_template('livres_list.html', title="R3.01 Dev Web avec Flask", livres=lesLivres)
 
 if __name__== "__main__":
     app.run()
